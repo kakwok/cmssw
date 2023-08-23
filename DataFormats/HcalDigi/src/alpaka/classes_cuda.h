64c16d6929b252ee9185f84ddca4c8448fc01ad3
kakwok <martin.kwok@cern.ch>
2023-08-23 23:29:19 +0200

    add classes

---
+++ DataFormats/HcalDigi/src/alpaka/classes_cuda.h
@@ -0,0 +1,5 @@
+#include "DataFormats/Common/interface/DeviceProduct.h"
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Portable/interface/Product.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiSoA.h"
+#include "DataFormats/HcalDigi/interface/alpaka/HcalDigiDeviceCollection.h"
---
+++ DataFormats/HcalDigi/src/alpaka/classes_serial.h
@@ -0,0 +1,4 @@
+#include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/Portable/interface/Product.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiHostCollection.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiSoA.h"
