afeb2eb562983248e6b9e6df9ca56621e19c1eb0
kakwok <martin.kwok@cern.ch>
2023-08-24 22:46:54 +0200

    Move serial backend out of alpaka

--- DataFormats/HcalDigi/src/alpaka/classes_serial.h
+++
@@ -1,4 +0,0 @@
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "DataFormats/Portable/interface/Product.h"
-#include "DataFormats/HcalDigi/interface/HcalDigiHostCollection.h"
-#include "DataFormats/HcalDigi/interface/HcalDigiSoA.h"
