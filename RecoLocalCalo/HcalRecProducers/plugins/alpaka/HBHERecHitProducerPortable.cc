9d84e640538b2f11d30b4f550a310dfc4bca5e2d
kakwok <martin.kwok@cern.ch>
2023-08-30 23:36:24 +0200

    Add alpaka plugin

---
+++ RecoLocalCalo/HcalRecProducers/plugins/alpaka/HBHERecHitProducerPortable.cc
@@ -0,0 +1,154 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+#include "DataFormats/HcalDigi/interface/alpaka/HcalDigiDeviceCollection.h"
+#include "DataFormats/HcalRecHit/interface/alpaka/HcalRecHitDeviceCollection.h"
+
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/Event.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/EventSetup.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/EDGetToken.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/EDPutToken.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/stream/EDProducer.h"
+
+
+//#include "SimpleAlgoGPU.h"
+#include "DeclsForKernels.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE{
+
+    class HBHERecHitProducerPortable : public stream::EDProducer<> {
+    public:
+      explicit HBHERecHitProducerPortable(edm::ParameterSet const&);
+      ~HBHERecHitProducerPortable() override;
+      static void fillDescriptions(edm::ConfigurationDescriptions&);
+    
+    private:
+      void produce(device::Event&, device::EventSetup const&) override;
+    
+      using IProductTypef01 = hcal::Phase1DigiDeviceCollection;
+      edm::EDGetTokenT<IProductTypef01> digisTokenF01HE_;
+    
+      using IProductTypef5 = hcal::Phase0DigiDeviceCollection;
+      edm::EDGetTokenT<IProductTypef5> digisTokenF5HB_;
+    
+      using IProductTypef3 = hcal::Phase1DigiDeviceCollection;
+      edm::EDGetTokenT<IProductTypef3> digisTokenF3HB_;
+    
+      using OProductType = hcal::RecHitDeviceCollection;
+      edm::EDPutTokenT<OProductType> rechitsM0Token_;
+    
+      const edm::ESGetToken<HcalRecoParamsWithPulseShapesGPU, HcalRecoParamsRcd> recoParamsToken_;
+      const edm::ESGetToken<HcalGainWidthsGPU, HcalGainWidthsRcd> gainWidthsToken_;
+      const edm::ESGetToken<HcalGainsGPU, HcalGainsRcd> gainsToken_;
+      const edm::ESGetToken<HcalLUTCorrsGPU, HcalLUTCorrsRcd> lutCorrsToken_;
+      const edm::ESGetToken<HcalConvertedPedestalWidthsGPU, HcalConvertedPedestalWidthsRcd> pedestalWidthsToken_;
+      const edm::ESGetToken<HcalConvertedEffectivePedestalWidthsGPU, HcalConvertedPedestalWidthsRcd>
+          effectivePedestalWidthsToken_;
+      const edm::ESGetToken<HcalConvertedPedestalsGPU, HcalConvertedPedestalsRcd> pedestalsToken_;
+      edm::ESGetToken<HcalConvertedEffectivePedestalsGPU, HcalConvertedPedestalsRcd> effectivePedestalsToken_;
+      const edm::ESGetToken<HcalQIECodersGPU, HcalQIEDataRcd> qieCodersToken_;
+      const edm::ESGetToken<HcalRespCorrsGPU, HcalRespCorrsRcd> respCorrsToken_;
+      const edm::ESGetToken<HcalTimeCorrsGPU, HcalTimeCorrsRcd> timeCorrsToken_;
+      const edm::ESGetToken<HcalQIETypesGPU, HcalQIETypesRcd> qieTypesToken_;
+      const edm::ESGetToken<HcalTopology, HcalRecNumberingRecord> topologyToken_;
+      const edm::ESGetToken<HcalDDDRecConstants, HcalRecNumberingRecord> recConstantsToken_;
+      const edm::ESGetToken<HcalSiPMParametersGPU, HcalSiPMParametersRcd> sipmParametersToken_;
+      const edm::ESGetToken<HcalSiPMCharacteristicsGPU, HcalSiPMCharacteristicsRcd> sipmCharacteristicsToken_;
+      const edm::ESGetToken<HcalChannelQualityGPU, HcalChannelQualityRcd> chQualProductToken_;
+      const edm::ESGetToken<HcalMahiPulseOffsetsGPU, JobConfigurationGPURecord> pulseOffsetsToken_;
+    
+      hcal::reconstruction::ConfigParameters configParameters_;
+      //hcal::reconstruction::OutputDataGPU outputGPU_;
+    };
+    
+    HBHERecHitProducerPortable::HBHERecHitProducerPortable(edm::ParameterSet const& ps)
+        : digisTokenF01HE_{consumes<IProductTypef01>(ps.getParameter<edm::InputTag>("digisLabelF01HE"))},
+          digisTokenF5HB_{consumes<IProductTypef5>(ps.getParameter<edm::InputTag>("digisLabelF5HB"))},
+          digisTokenF3HB_{consumes<IProductTypef3>(ps.getParameter<edm::InputTag>("digisLabelF3HB"))},
+          rechitsM0Token_{produces<OProductType>(ps.getParameter<std::string>("recHitsLabelM0HBHE"))},
+          recoParamsToken_{esConsumes()},
+          gainWidthsToken_{esConsumes()},
+          gainsToken_{esConsumes()},
+          lutCorrsToken_{esConsumes()},
+          pedestalWidthsToken_{esConsumes()},
+          effectivePedestalWidthsToken_{esConsumes()},
+          pedestalsToken_{esConsumes()},
+          qieCodersToken_{esConsumes()},
+          respCorrsToken_{esConsumes()},
+          timeCorrsToken_{esConsumes()},
+          qieTypesToken_{esConsumes()},
+          topologyToken_{esConsumes()},
+          recConstantsToken_{esConsumes()},
+          sipmParametersToken_{esConsumes()},
+          sipmCharacteristicsToken_{esConsumes()},
+          chQualProductToken_{esConsumes()},
+          pulseOffsetsToken_{esConsumes()} {
+      configParameters_.maxTimeSamples = ps.getParameter<uint32_t>("maxTimeSamples");
+      configParameters_.kprep1dChannelsPerBlock = ps.getParameter<uint32_t>("kprep1dChannelsPerBlock");
+      configParameters_.sipmQTSShift = ps.getParameter<int>("sipmQTSShift");
+      configParameters_.sipmQNTStoSum = ps.getParameter<int>("sipmQNTStoSum");
+      configParameters_.firstSampleShift = ps.getParameter<int>("firstSampleShift");
+      configParameters_.useEffectivePedestals = ps.getParameter<bool>("useEffectivePedestals");
+      if (configParameters_.useEffectivePedestals) {
+        effectivePedestalsToken_ = esConsumes();
+      }
+    
+      configParameters_.meanTime = ps.getParameter<double>("meanTime");
+      configParameters_.timeSigmaSiPM = ps.getParameter<double>("timeSigmaSiPM");
+      configParameters_.timeSigmaHPD = ps.getParameter<double>("timeSigmaHPD");
+      configParameters_.ts4Thresh = ps.getParameter<double>("ts4Thresh");
+    
+      configParameters_.applyTimeSlew = ps.getParameter<bool>("applyTimeSlew");
+      auto const tzeroValues = ps.getParameter<std::vector<double>>("tzeroTimeSlewParameters");
+      auto const slopeValues = ps.getParameter<std::vector<double>>("slopeTimeSlewParameters");
+      auto const tmaxValues = ps.getParameter<std::vector<double>>("tmaxTimeSlewParameters");
+    
+      configParameters_.tzeroTimeSlew = tzeroValues[HcalTimeSlew::Medium];
+      configParameters_.slopeTimeSlew = slopeValues[HcalTimeSlew::Medium];
+      configParameters_.tmaxTimeSlew = tmaxValues[HcalTimeSlew::Medium];
+    
+      auto threadsMinimize = ps.getParameter<std::vector<uint32_t>>("kernelMinimizeThreads");
+      configParameters_.kernelMinimizeThreads[0] = threadsMinimize[0];
+      configParameters_.kernelMinimizeThreads[1] = threadsMinimize[1];
+      configParameters_.kernelMinimizeThreads[2] = threadsMinimize[2];
+    }
+    
+    HBHERecHitProducerPortable::~HBHERecHitProducerPortable() {}
+    
+    void HBHERecHitProducerPortable::fillDescriptions(edm::ConfigurationDescriptions& cdesc) {
+      edm::ParameterSetDescription desc;
+      desc.add<uint32_t>("maxTimeSamples", 10);
+      desc.add<uint32_t>("kprep1dChannelsPerBlock", 32);
+      desc.add<edm::InputTag>("digisLabelF01HE", edm::InputTag{"hcalRawToDigiGPU", "f01HEDigisGPU"});
+      desc.add<edm::InputTag>("digisLabelF5HB", edm::InputTag{"hcalRawToDigiGPU", "f5HBDigisGPU"});
+      desc.add<edm::InputTag>("digisLabelF3HB", edm::InputTag{"hcalRawToDigiGPU", "f3HBDigisGPU"});
+      desc.add<std::string>("recHitsLabelM0HBHE", "recHitsM0HBHE");
+      desc.add<int>("sipmQTSShift", 0);
+      desc.add<int>("sipmQNTStoSum", 3);
+      desc.add<int>("firstSampleShift", 0);
+      desc.add<bool>("useEffectivePedestals", true);
+    
+      desc.add<double>("meanTime", 0.f);
+      desc.add<double>("timeSigmaSiPM", 2.5f);
+      desc.add<double>("timeSigmaHPD", 5.0f);
+      desc.add<double>("ts4Thresh", 0.0);
+    
+      desc.add<bool>("applyTimeSlew", true);
+      desc.add<std::vector<double>>("tzeroTimeSlewParameters", {23.960177, 11.977461, 9.109694});
+      desc.add<std::vector<double>>("slopeTimeSlewParameters", {-3.178648, -1.5610227, -1.075824});
+      desc.add<std::vector<double>>("tmaxTimeSlewParameters", {16.00, 10.00, 6.25});
+      desc.add<std::vector<uint32_t>>("kernelMinimizeThreads", {16, 1, 1});
+    
+      cdesc.addWithDefaultLabel(desc);
+    }
+    
+    void HBHERecHitProducerPortable::produce(device::Event& event, device::EventSetup const& setup) {
+      //event.emplace(event, rechitsM0Token_, std::move(outputGPU_.recHits));
+    }
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/MakerMacros.h"
+DEFINE_FWK_ALPAKA_MODULE(HBHERecHitProducerPortable);
