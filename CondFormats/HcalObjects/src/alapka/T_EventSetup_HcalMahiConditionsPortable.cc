4a56fc0bcbac325f04b97b788c602a6e085c1c8d
kakwok <martin.kwok@cern.ch>
2023-09-28 09:01:54 +0200

    Add MahiConditionsSoA

---
+++ CondFormats/HcalObjects/src/alapka/T_EventSetup_HcalMahiConditionsPortable.cc
@@ -0,0 +1,4 @@
+#include "CondFormats/HcalObjects/interface/HcalMahiConditionsPortable.h"
+#include "FWCore/Utilities/interface/typelookup.h"
+
+TYPELOOKUP_ALPAKA_DATA_REG(HcalMahiConditionsPortableDevice);
