8a14d86601a537f1153e0e18eaee30b3b32260c4
kakwok <martin.kwok@cern.ch>
2023-11-15 23:57:04 +0100

    WIP: HcalRecoParamWithPulseShape compiles

---
+++ CondFormats/HcalObjects/interface/alpaka/HcalRecoParamWithPulseShapePortable.h
@@ -0,0 +1,28 @@
+#ifndef CondFormats_HcalObjects_interface_alpaka_HcalRecoParamWithPulseShapePortable_h
+#define CondFormats_HcalObjects_interface_alpaka_HcalRecoParamWithPulseShapePortable_h
+
+#include "CondFormats/HcalObjects/interface/HcalRecoParamWithPulseShapePortable.h"
+#include "CondFormats/HcalObjects/interface/HcalRecoParamWithPulseShapeSoA.h"
+#include "DataFormats/Portable/interface/alpaka/PortableCollection.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/memory.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+  using HcalRecoParamWithPulseShapeDevice = HcalRecoParamWithPulseShapeT<Device>;
+}
+
+namespace cms::alpakatools {
+  template<>
+  struct CopyToDevice<HcalRecoParamWithPulseShapeHost> {
+    template <typename TQueue>
+    static auto copyAsync(TQueue& queue, HcalRecoParamWithPulseShapeHost const& hostProduct) {
+      using RecoParamCopy = CopyToDevice<HcalRecoParamWithPulseShapeHost::RecoParamCollection>;
+      using PulseShapeCopy = CopyToDevice<HcalRecoParamWithPulseShapeHost::PulseShapeCollection>;
+      //using TDevice = typename alpaka::trait::DevType<TQueue>::type;
+      using TDevice = alpaka::Dev<TQueue>;
+      return HcalRecoParamWithPulseShapeT<TDevice>(RecoParamCopy::copyAsync(queue, hostProduct.recoParam()),
+                                                                 PulseShapeCopy::copyAsync(queue, hostProduct.pulseShape()));
+    }
+  };
+}
+#endif
