4781fe7a5f4fcd7d200c1a2beab2c584b770d812
Martin <ka_hei_martin_kwok@brown.edu>
2025-04-02 09:52:35 -0500

    Include RetryAction in SonicClientBase

--- HeterogeneousCore/SonicCore/src/RetrySameServerAction.cc
+++ HeterogeneousCore/SonicCore/src/RetrySameServerAction.cc
@@ -1,11 +1,16 @@
 #include "HeterogeneousCore/SonicCore/interface/RetrySameServerAction.h"
 #include "HeterogeneousCore/SonicCore/interface/SonicClientBase.h"
 
-void RetrySameServerAction::retry()  {
-     ++tries_;
-    //if max retries has not been exceeded, call evaluate again
-    if (tries_ < allowedTries_) {
-      eval();
-      return;
-    }
+void RetrySameServerAction::retry() {
+  ++tries_;
+  //if max retries has not been exceeded, call evaluate again
+  if (tries_ < allowedTries_) {
+    eval();
+    return;
+  }else{
+     shouldRetry_ = false;  // Flip flag when max retries are reached
+     edm::LogInfo("RetrySameServerAction") << "Max retry attempts reached. No further retries.";
+  }
 }
+
+DEFINE_EDM_PLUGIN(RetryActionFactory, RetrySameServerAction, "RetrySameServerAction");
